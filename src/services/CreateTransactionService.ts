0f8a9c5b7376157f72a02593193b09a400ab7a69
Denner Ribeiro <dnralmeida@gmail.com>
2021-04-22 21:35:23 -0300

    Creates, lists and validates the balance of transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Pesquisa se o tipo está contido nas opções do vetor e retorna true ou false
+    if (!['income', 'outcome'].includes(type)) throw Error('Transaction type is not valid!');
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) throw Error('The outcome cannot be greater than income!')
+
+    const transaction = this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
   }
 }
 
